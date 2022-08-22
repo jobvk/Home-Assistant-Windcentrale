82941958e79ecfffbd5871431fab0b54b7dd5021
Job van Koeveringe <job@vankoeveringe.com>
2022-08-22 19:40:31 +0200

    Small changes

--- custom_components/windcentrale/sensor.py
+++ custom_components/windcentrale/sensor.py
@@ -11,6 +11,7 @@ from homeassistant.components.sensor import (
     ATTR_LAST_RESET,
     CONF_STATE_CLASS,
 )
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add sensors for passed config_entry in HA."""
     wind = hass.data[DOMAIN][config_entry.entry_id]
@@ -135,7 +136,7 @@ class LiveSensor(SensorBase):
         elif self.type == "windspeed" or self.type == "powertotal" or self.type == "powerpershare" or self.type == "powerpercentage" or self.type == "rpm":
             attr[CONF_STATE_CLASS] = SensorStateClass.MEASUREMENT
         elif self.type == "energy" or self.type == "yearproduction":
-            attr[ATTR_LAST_RESET] = datetime(datetime.today().year, 1 ,1)
+            attr[ATTR_LAST_RESET] = datetime(datetime.now().year, 1, 1)
             attr[CONF_STATE_CLASS] = SensorStateClass.TOTAL
         return attr
 
